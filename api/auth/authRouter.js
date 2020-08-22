32cc43ac981841d6d7b53ce048e1e79988c4f1ae
Drew Ross <mail.drew.ross@gmail.com>
2020-08-22 10:51:16 -0700

    Add middleware to require properties in req.body.

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const knex = require('knex');
-
+const { requireInBody } = require('../../utils/middleware');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'auth' });
 });
 
-// router.post('/register', (req, res) => {
-
-// })
+router.post('/register', requireInBody(['username', 'password']), (req, res) => {
+  res.send(200);
+});
 
 module.exports = router;
---
+++ utils/middleware.js
@@ -0,0 +1,19 @@
+module.exports = {
+  requireInBody
+}
+
+function requireInBody(keys) {
+  return (req, res, next) => {
+    const missing = [];
+    keys.forEach(key => {
+      if (!req.body.hasOwnProperty(key)) {
+        missing.push(key);
+      }
+    });
+    if (missing.length > 0) {
+      res.status(400).json({ message: `Please include: ${missing.join(', ')}` });
+    } else {
+      next();
+    }
+  };
+};
