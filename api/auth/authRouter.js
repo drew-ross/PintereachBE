c811e1799a14312959940111f7c8944d2c37c6f1
Drew Ross <mail.drew.ross@gmail.com>
2020-08-22 11:00:43 -0700

    Add authorization middleware to check jwt.

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const knex = require('knex');
+const { signToken, restricted } = require('../../utils/jsonwebtoken');
 const { requireInBody } = require('../../utils/middleware');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'auth' });
 });
 
-router.post('/register', requireInBody(['username', 'password']), (req, res) => {
+router.post('/register', restricted, requireInBody(['username', 'password']), (req, res) => {
   res.send(200);
 });
 
--- utils/jsonwebtoken.js
+++ utils/jsonwebtoken.js
@@ -3,7 +3,8 @@ const secrets = require('../config/secrets');
 
 module.exports = {
   signToken,
-}
+  restricted
+};
 
 function signToken(user) {
   const payload = {
@@ -16,3 +17,19 @@ function signToken(user) {
   };
   return jwt.sign(payload, secret, options);
 }
+
+function restricted(req, res, next) {
+  const token = req.headers.Authorization;
+  if (token) {
+    jwt.verify(token, constants.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ message: 'Please provide credentials.' });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'Please provide credentials.' });
+  }
+};
