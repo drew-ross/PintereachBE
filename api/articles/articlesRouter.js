4cb4b19cce38729f822510821049597d4989eb1a
Drew Ross <mail.drew.ross@gmail.com>
2020-08-24 13:24:28 -0700

    Add logic for PUT, DELETE, POST endpoints and db methods.

--- api/articles/articlesRouter.js
+++ api/articles/articlesRouter.js
@@ -14,16 +14,43 @@ router.get('/', restricted, (req, res) => {
     .catch(err => res.status(500).json({ message: 'There was a problem getting articles.', error: err.message }));
 });
 
-router.post('/', restricted, (req, res) => {
-  res.status(200).end();
+router.post('/', restricted, requireInBody(['title']), (req, res) => {
+  const user_id = req.decodedToken.subject;
+  const article = req.body;
+  articlesDb.addByUser(user_id, article)
+    .then(article => {
+      res.status(201).json(article);
+    })
+    .catch(err => res.status(500).json({ message: 'There was a problem creating the article.', error: err.message }));
 });
 
-router.put('/:id', restricted, (req, res) => {
-  res.status(200).end();
+router.put('/:id', restricted, requireInBody([]), (req, res) => {
+  const user_id = req.decodedToken.subject;
+  const article = req.body;
+  const id = req.params.id;
+  articlesDb.updateByUser(user_id, id, article)
+    .then(article => {
+      if (article) {
+        res.status(200).json(article);
+      } else {
+        res.status(404).json({ message: 'Article not found.' });
+      }
+    })
+    .catch(err => res.status(500).json({ message: 'There was a problem updating the article.', error: err.message }));
 });
 
 router.delete('/:id', restricted, (req, res) => {
-  res.status(200).end();
+  const user_id = req.decodedToken.subject;
+  const id = req.params.id;
+  articlesDb.removeByUser(user_id, id)
+    .then(id => {
+      if (id) {
+        res.status(200).json({ message: 'Deleted.', id });
+      } else {
+        res.status(404).json({ message: 'Article not found.' });
+      }
+    })
+    .catch(err => res.status(500).json({ message: 'There was a problem deleting the article.', error: err.message }));
 });
 
 module.exports = router;
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,10 +1,13 @@
 module.exports = {
   requireInBody
-}
+};
 
 function requireInBody(keys) {
   return (req, res, next) => {
     const missing = [];
+    if (keys.length === 0) {
+      res.status(400).json({ message: `Please include request body.` });
+    }
     keys.forEach(key => {
       if (!req.body.hasOwnProperty(key)) {
         missing.push(key);
