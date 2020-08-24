d23816e423572f604708901508c3fd785fbbc3b4
Drew Ross <mail.drew.ross@gmail.com>
2020-08-24 16:07:24 -0700

    Change truncate users to del users to deal with postgres foreign key constraint.

--- data/seeds/00_truncate.js
+++ data/seeds/00_truncate.js
@@ -1,8 +1,5 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('articles').truncate()
-    .then(() => {
-      return knex('users').truncate();
-    });
+  return knex('users').del()
 };
