ed4c410f3068dc3548aa88022252ba776e23209a
Drew Ross <mail.drew.ross@gmail.com>
2020-08-24 13:27:28 -0700

    Add cors dependency.

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const authRouter = require('./auth/authRouter');
 const articlesRouter = require('./articles/articlesRouter');
 
 const server = express();
 
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
